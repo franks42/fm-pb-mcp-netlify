e7e1186b1b2774e7f19d7bf18ad3efa09b8ee7a1
FrankS <frank.siebenlist@gmail.com>
2025-06-09 22:23:31 -0700

    changing from python to js :-(

---
+++ netlify/functions/hash_price.js
@@ -0,0 +1,34 @@
+exports.handler = async (event, context) => {
+  try {
+    const url = "https://www.figuremarkets.com/service-hft-exchange/api/v1/trades/HASH-USD?size=1";
+    
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    const data = await response.json();
+    
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(data)
+    };
+    
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
+};
